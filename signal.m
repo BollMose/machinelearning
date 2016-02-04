aeb1ca7954ed559cc7540c1b0b0ea1ef90f43acb
DxxxBell <DienBell@gmail.com>
2016-02-04 13:59:52 +0800

    Update signal.m

--- signal.m
+++ signal.m
@@ -1,3 +1,5 @@
+
+# simple fft
 t=0:0.001:2;
 x=2*sin(20*pi*t) + sin(100*pi*t);
 subplot(2,1,1);
@@ -11,3 +13,13 @@ y=fft(x);
 plot(1000*t,abs(y));
 xlabel("Frequency");
 ylabel("Signal amplitude");
+
+# fft
+n = [0:149];
+x1 = cos(2*pi*n/10);
+N = 2048;
+X = abs(fft(x1,N));
+X = fftshift(X);
+F = [-N/2:N/2-1]/N;
+plot(F,X);
+xlabel(’frequency / f s’)
